a3beee53e24786a4c1dc2198d4f915943e26c40e
Junninho Thomas <junninhothomas1998@gmail.com>
2020-11-30 15:33:48 -0500

    implemented upload and gallery

--- src/components/pages/Gallery/index.js
+++ src/components/pages/Gallery/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Gallery.css';
 
-export default class Gallery extends React.Component {
-    render() {
-        return (
-            <div><h1>Gallery</h1></div>
-        )
+const Gallery = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    (async () => {
+      const response = await fetch('https://obscure-stream-03189.herokuapp.com/images')
+      .then(r => r.json())
+        .catch(e =>{
+          console.error(e);
+        });
+
+
+      if (mounted) {
+        setImages(response);
+      }
+    })()
+    return () => {
+      mounted = false;
     }
+  },[])
+
+  return (
+    <div className="Gallery">
+      <div><h1>Gallery</h1></div>
+      {images.map(img => <img key={img.fileName} src={img.url} />)}
+    </div>
+  )
 }
+
+export default Gallery;
--- src/components/pages/Upload/index.js
+++ src/components/pages/Upload/index.js
@@ -1,61 +1,113 @@
 import React from 'react';
-import EXIF from "exif-js";
-import { Button } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+
+import axios from 'axios';
 
 import './Upload.css'
 
 export default class Upload extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fileName: '',
-            exif: ''
-        }
-
-        this.handleChange = this.handleChange.bind(this);
+  constructor(props){
+    super(props);
+    this.state = {
+      successMessage : "",
+      fileName: "",
+      file: null,
+      creationDate: null,
+      cameraModel: "",
+      category: "",
+      size: 0
     }
-    handleChange({
-        target: {
-            files: [file]
-        }
-    }) {
-        let exifd = '';
-        if (file && file.name) {
-            this.setState({fileName: file.name});
-            var that = this;
-            EXIF.getData(file, function() {
-                var exifData = EXIF.pretty(this);
-                if (exifData) {
-                    console.log(exifData);
-                    that.setState({exif: exifData});
-                // console.log(EXIF.getTag(this, "Orientation"));
-                } else {
-                    that.setState({exif: 'No EXIF data found in image'})
-                    console.log("No EXIF data found in image");
-              }
-            })
-        }
-        this.setState({exif: exifd});
-        console.log(exifd)
-        }
+  }
+  
+  handleChange = (ev) => {
+    this.setState({success: false, url : ""});
+    let file = ev.target.files[0];
+    this.setState({file, fileName: file.name, creationDate: file.lastModified, size: file.size});
+  }
 
-    render() {
-        return (
-            <div className="Upload">
-                <h1>Upload</h1>
-                <Button variant="contained" color="primary" href="/upload" component="label">
-                    Choose File
-                    <input
-                        type="file"
-                        id="file"
-                        accept=".jpg, .png, .heif, .heic"
-                        onChange={this.handleChange}
-                        hidden
-                    />
-                </Button>
-                <p>{this.state.fileName}</p>
-                <div>{this.state.exif}</div>
-            </div>
-        )
-    }
+  handleDateChange = (date) => {
+    this.setState({creationDate: date})
+  }
+
+  handleTextChange = (field) => (ev) => {
+    this.setState({[field]: ev.target.value});
+  }
+
+  // Perform the upload
+  handleUpload = (ev) => {
+    let file = this.state.file;
+    console.log(file)
+    console.log("Preparing the upload");
+    const data = new FormData();
+    data.append('file', file);
+    data.append('fileName', this.state.fileName);
+    data.append('creationDate', this.state.creationDate);
+    data.append('cameraModel', this.state.cameraModel);
+    data.append('category', this.state.category);
+    data.append('size', this.state.size);
+    axios.post("https://obscure-stream-03189.herokuapp.com/upload", data).then(res => {
+      console.log(res.statusText);
+      this.setState({successMessage: "File Uploaded!"})
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+  
+  
+  render() {
+    return (
+      <div className="Upload">
+        <center>
+          <h1>UPLOAD A FILE</h1>
+          <Button variant="contained" color="primary" href="/upload" component="label">
+            Choose File
+            <input
+              type="file"
+              id="file"
+              accept=".jpg, .png, .heif, .heic"
+              onChange={this.handleChange}
+              ref={(ref) => { this.uploadInput = ref; }}
+              hidden
+            />
+          </Button>
+          <p>{this.state.fileName}</p>
+          <br/>
+          <div>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                required
+                disableToolbar
+                variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="Creation Date"
+                value={this.state.creationDate}
+                onChange={this.handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+            </MuiPickersUtilsProvider>
+            <br/>
+            <TextField required label="File Name" value={this.state.fileName} onChange={this.handleTextChange('fileName')} />
+            <br/>
+            <TextField required label="Camera Model" value={this.state.cameraModel} onChange={this.handleTextChange('cameraModel')} />
+            <br/>
+            <TextField required label="Category" value={this.state.category} onChange={this.handleTextChange('category')} />
+            <br/>
+            <br/>
+          </div>
+          <button disabled={this.state.file == null} onClick={this.handleUpload}>UPLOAD</button>
+          <div style={{padding:50}}>
+            <h3 style={{color: 'green'}}>{this.state.successMessage}</h3>
+            <br/>
+          </div>
+        </center>
+      </div>
+    );
+  }
 }
